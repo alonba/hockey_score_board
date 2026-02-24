fd785e154f70f665ab04697b3511601eb08fccbb
Alon Ben-Arieh <alonbenarieh@gmail.com>
2026-02-24 12:21:03 +0200

    PWA

--- app.js
+++ app.js
@@ -860,4 +860,11 @@
         loadState();
     });
 
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('./service-worker.js')
+                .catch(err => console.error('Service worker registration failed:', err));
+        });
+    }
+
 })();
---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'scoreboard-v2';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './favicon.svg',
+  './icon-192.png',
+  './icon-512.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
